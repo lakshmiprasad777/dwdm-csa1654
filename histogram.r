9f0efffdf7302cd46cebb12a44247103266a238f
lakshmiprasad777 <113926683+lakshmiprasad777@users.noreply.github.com>
2022-09-28 10:26:01 +0530

    Add files via upload

---
+++ histogram.r
@@ -0,0 +1,9 @@
+######code for creating an histogram#####
+hist(AirPassengers,
+     main="Histogram for Air Passengers",
+     xlab="Passengers",
+     border="blue",
+     col="green",
+     xlim=c(100,700),
+     las=1,
+     breaks=5)
